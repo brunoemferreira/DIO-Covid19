0fa8e61bfb5b1fefb71de6d8f07d3466cc76b4c6
brunoemferreira <brunoemf@gmail.com>
2021-04-26 13:20:31 -0300

    Creating App Components

---
+++ covid19-dio/src/containers/Main/Components/Board/index.jsx
@@ -0,0 +1,18 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import { Grid, Skeleton } from '../../Components';
+
+
+function Board({ data }) {
+  const { cases, todayDeaths, recovered, deaths, todayCases } = data;
+
+  const getValue = (value) => value ? value : <Skeleton variant="text" width={182} height={60} />
+
+  return (
+    <Grid container spacing={4}>
+      <Grid item xs={12} md={3}>
+        <Card value={getValue(cases)} label="Total de casos" color="#5d78ff" />
+      </Grid>
+    </Grid>
+  )
+}
--- covid19-dio/src/containers/Main/index.jsx
+++ covid19-dio/src/containers/Main/index.jsx
@@ -1,10 +1,27 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState, useCallback } from 'react';
+import Api from '../../api';
+
+import { Container } from './style';
 
 function Main() {
+  const [data, setData] = useState({});
+  const [country, setCountry] = useState('brazil');
+
+  const getCovidData = useCallback(() => {
+    Api.getCountry(country)
+      .then(data => setData(data))
+  })
+
+  useEffect(() => {
+    getCovidData(country)
+  }, [getCovidData, country])
+
   return (
-    <div>
-      Teste
-    </div>
+    <Container>
+      <div className="mb-2">
+
+      </div>
+    </Container>
   )
 }
 
